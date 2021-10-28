70c090ce270dbf9a9bf7c4ffcd731507764d171d
brollix <brolloagus@gmail.com>
2021-10-28 03:32:39 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar.js';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					<h1>Soy Agus</h1>
-					<h3>Esta es mi React App</h3>
-					en el siguiente link les dejo mi repositorio:
-				</p>
-				<a
-					className="App-link"
-					href="https://github.com/Brollix"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					GitHub
-				</a>
-			</header>
+			<Navbar />
 		</div>
 	);
 }
---
+++ src/components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+	{
+		title: 'Home',
+		url: '#',
+		cName: 'nav-links',
+	},
+	{
+		title: 'Products',
+		url: '#',
+		cName: 'nav-links',
+	},
+	{
+		title: 'Contact Us',
+		url: '#',
+		cName: 'nav-links',
+	},
+];
