22bf94cb75c16a4d7f91ef95f20576361ba65213
brollix <brolloagus@gmail.com>
2021-10-27 23:17:20 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Soy Agustin Brollo, en el siguiente link en celeste les dejo
+					mi repositorio de GitHub:
+				</p>
+				<a
+					className="App-link"
+					href="https://github.com/Brollix"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Brollix
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
