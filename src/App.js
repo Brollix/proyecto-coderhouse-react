bf93aa566d79b51f2f659505853ff73d7127ff1b
brollix <brolloagus@gmail.com>
2021-11-02 03:13:26 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
+import CartWidget from './components/CartWidget.js';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<CartWidget />
 		</div>
 	);
 }
---
+++ src/components/CartWidget.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartWidget() {
+	return <div>asdfasdf</div>;
+}
+
+export default CartWidget;
