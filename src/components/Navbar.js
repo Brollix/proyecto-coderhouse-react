58c856a6f9f2a8da09e4a938540dc6c38ff33be6
brollix <brolloagus@gmail.com>
2021-11-04 02:22:47 -0300

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import { MenuItems } from './MenuItems';
+import React from 'react';
 import './Navbar.css';
 
-class Navbar extends Component {
-	render() {
-		return (
-			<nav className="NavbarItems">
-				<span className="fas fa-desktop" />
-				<h1 className="navbar-logo">BlackComponents</h1>
-				<div className="menu-icon">
-					<i className="fas fa-bars" />
-				</div>
+export default function Navbar() {
+	return (
+		<nav className="NavbarItems">
+			<h1 className="navbar-logo">
+				<i className="fas fa-desktop" />
+				BlackComponents
+			</h1>
 
-				<div className="container">
-					<span className="fas fa-shopping-cart"></span>
-					<h3 className="cart-count">0</h3>
-				</div>
-			</nav>
-		);
-	}
+			<div className="container">
+				<span className="fas fa-shopping-cart" />
+			</div>
+
+			<div className="menu-icon">
+				<i className="fas fa-bars" />
+			</div>
+		</nav>
+	);
 }
-
-export default Navbar;
