a60b885047bde15a1a8f18f26834059e934cb7c5
brollix <brolloagus@gmail.com>
2021-11-09 23:27:31 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar.js';
-import ItemListContainer from './components/ItemListContainer';
+import Navbar from './components/Navbar/Navbar.js';
+import ItemList from './components/ItemList/ItemList.js';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<ItemListContainer />
+			<ItemList />
 		</div>
 	);
 }
