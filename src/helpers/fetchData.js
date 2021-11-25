249e54f4b7e6670966e212ec7185f33c5c0ebf57
Agus Brollo <brolloagus@gmail.com>
2021-11-25 00:32:44 -0300

    Update fetchData.js

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,5 +1,8 @@
 import { stock } from '../data/stock';
-
-export const fetchData = async () => {
-	fetch(stock);
+export const fetchData =  () => {
+    return new Promise((res, rej)=> {
+        setTimeout(() => {
+            return res(stock)
+        }, 3000);
+    })
 };
