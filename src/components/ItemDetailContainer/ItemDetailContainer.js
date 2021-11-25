cf84a3f2e43573d774046aff6b8cbef36a6740a8
Agus Brollo <brolloagus@gmail.com>
2021-11-25 00:32:07 -0300

    Update ItemDetailContainer.js

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,64 +3,64 @@ import { fetchData } from '../../helpers/fetchData';
 import Card from '@mui/material/Card';
 import { CardMedia, Typography } from '@mui/material';
 import { Button, ButtonGroup } from '@mui/material';
-
 export const ItemDetailContainer = () => {
-	const [productos, setProductos] = useState([]);
-	const [index, setIndex] = useState(0);
-
-	const handleAnterior = () => {
-		if (index > 0) {
-			setIndex(index - 1);
-		}
-	};
-	const handleSiguiente = () => {
-		if (index < productos.length()) {
-			setIndex(index + 1);
-		}
-	};
-
-	useEffect(() => {
-		fetchData()
-			.then((resp) => {
-				setProductos(resp);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
-
-	return (
-		<div>
-			<Card key={productos[index].id} className="card">
-				<CardMedia
-					className="media"
-					component="img"
-					image={
-						require(`../../data/img/${productos[index].Imagen}`)
-							.default
-					}
-					alt={productos[index].Imagen}
-				/>
-				<Typography variant="body2" color="initial">
-					{productos[index].Tipo}
-				</Typography>
-				<Typography
-					gutterBottom
-					variant="h4"
-					color="initial"
-					component="div"
-				>
-					{productos[index].Marca + ' ' + productos[index].Serie}
-				</Typography>
-				<Typography variant="body1" color="initial">
-					USD${productos[index].Precio}
-				</Typography>
-
-				<ButtonGroup variant="contained" color="primary" aria-label="">
-					<Button onClick={handleAnterior}> Anterior </Button>
-					<Button onClick={handleSiguiente}> Siguiente </Button>
-				</ButtonGroup>
-			</Card>
-		</div>
-	);
+    const [productos, setProductos] = useState([]);
+    const [index, setIndex] = useState(0);
+    const handleAnterior = () => {
+        if (index > 0) {
+            setIndex(index - 1);
+        }
+    };
+    const handleSiguiente = () => {
+        if (index < productos.length()) {
+            setIndex(index + 1);
+        }
+    };
+    useEffect(() => {
+        fetchData()
+            .then((resp) => {
+                console.log(resp)
+                setProductos(resp);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [productos]);
+    return (
+        productos && productos.length ? (
+            productos.map(prod => (
+                <div>
+                    <Card key={prod.id} className="card">
+                        <CardMedia
+                            className="media"
+                            component="img"
+                            image={
+                                require(`../../data/img/${prod.Imagen}`)
+                                    .default
+                            }
+                            alt={prod.Imagen}
+                        />
+                        <Typography variant="body2" color="initial">
+                            {prod.Tipo}
+                        </Typography>
+                        <Typography
+                            gutterBottom
+                            variant="h4"
+                            color="initial"
+                            component="div"
+                        >
+                            {prod.Marca + ' ' + prod.Serie}
+                        </Typography>
+                        <Typography variant="body1" color="initial">
+                            USD$ {prod.Precio}
+                        </Typography>
+                        <ButtonGroup variant="contained" color="primary" aria-label="">
+                            <Button onClick={handleAnterior}> Anterior </Button>
+                            <Button onClick={handleSiguiente}> Siguiente </Button>
+                        </ButtonGroup>
+                    </Card>
+                </div>
+            ))
+        ) : <></>
+    );
 };
