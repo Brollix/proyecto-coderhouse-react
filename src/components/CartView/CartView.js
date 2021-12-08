033f7082c08ffd7f6c7e770514f409061cc404b3
brollix <brolloagus@gmail.com>
2021-12-08 19:49:33 -0300

    CartView Funcionalidad

--- src/components/CartView/CartView.js
+++ src/components/CartView/CartView.js
@@ -1,19 +1,36 @@
-import { Paper, Typography, Button } from '@mui/material';
+import { Typography, Button, Card } from '@mui/material';
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 import './CartView.css';
 
 export const CartView = () => {
-	const { cart, emptyCart } = useContext(CartContext);
+	const { cart, emptyCart, removeFromCart } = useContext(CartContext);
 
 	return (
-		<Paper>
-			{cart.map((prod) => {
-				return <Typography>{prod.serie}</Typography>;
-			})}
-			<Button variant="text" color="error" onClick={emptyCart}>
-				Vaciar Carrito
-			</Button>
-		</Paper>
+		<>
+			<Card>
+				{cart.map((prod) => {
+					return (
+						<>
+							<Typography>{prod.marca}</Typography>
+							<Typography>{prod.serie}</Typography>
+							{/* <Button variant="text" color="primary">
+								Eliminar
+							</Button> */}
+						</>
+					);
+				})}
+			</Card>
+			{cart.length > 0 ? (
+				<Button variant="contained" color="error" onClick={emptyCart}>
+					Vaciar Carrito
+				</Button>
+			) : (
+				<Button variant="text" color="primary">
+					<Link to="/">Volver al Inicio</Link>
+				</Button>
+			)}
+		</>
 	);
 };
