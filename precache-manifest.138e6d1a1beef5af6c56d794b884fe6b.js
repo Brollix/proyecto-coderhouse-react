dca6c2bb7ca5c622eafc5a59f72cff13522a9e18
brollix <brolloagus@gmail.com>
2021-12-19 00:33:58 -0300

    Updates

---
+++ precache-manifest.138e6d1a1beef5af6c56d794b884fe6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95794ab2f003ac64a296cb44d7bcbd4",
+    "url": "/proyecto-coderhouse-react/index.html"
+  },
+  {
+    "revision": "0a11bc0b76cd949fb429",
+    "url": "/proyecto-coderhouse-react/static/css/main.2b3f28e0.chunk.css"
+  },
+  {
+    "revision": "6917e04e62bf4e5f3914",
+    "url": "/proyecto-coderhouse-react/static/js/2.c432d170.chunk.js"
+  },
+  {
+    "revision": "428b38507086d485d87e5cbd762b7c59",
+    "url": "/proyecto-coderhouse-react/static/js/2.c432d170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a11bc0b76cd949fb429",
+    "url": "/proyecto-coderhouse-react/static/js/main.797b7200.chunk.js"
+  },
+  {
+    "revision": "9cf5c8f4b9964b2d4603",
+    "url": "/proyecto-coderhouse-react/static/js/runtime-main.4bc8b79c.js"
+  }
+]);
